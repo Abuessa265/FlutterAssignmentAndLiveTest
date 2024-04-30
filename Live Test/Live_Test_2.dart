d2481f1c8eb733acfcbf5828372f25b12164d2a5
Abuessa265 <abuessastudent1999@gmail.com>
2024-04-30 23:54:46 +0600

    add live text 2

--- Live Test/Live_Test_2.dart
+++ Live Test/Live_Test_2.dart
@@ -1,9 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// main() {
-//   runApp(const myApp());
-// }
+
 
 void main() {
   runApp(
